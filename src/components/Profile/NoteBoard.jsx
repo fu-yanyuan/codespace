d2645cef09393bc4b2ad8d317d8950a6e2e515c2
Yanyuan <fu@Yanyuans-Mac-mini.local>
2024-06-22 02:40:39 +0900

    feat: init NoteBoard

---
+++ src/components/Profile/NoteBoard.jsx
@@ -0,0 +1,13 @@
+const NoteBoard = () => {
+  return (
+    <div className="rounded-lg min-w-max max-w-full w-full bg-neutral-800">
+      <div className="h-[180px] min-h-[180px] w-full p-4">
+        <h1>
+          this is notes borad.
+        </h1>         
+      </div>
+    </div>
+  )
+}
+
+export default NoteBoard
