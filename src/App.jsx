e2754639f7844fba7d0c1c73f21d1aef2e8fbc4d
fu-yanyuan <yanyuan@g.ecc.u-tokyo.ac.jp>
2024-08-01 01:20:46 +0900

    feat(ui): add BaseNavBar and BaseFooter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import './App.css'
 import AuthPage from './components/Auth/AuthPage'
 import { useAuthContext } from './contexts/AuthContext'
 import Dashboard from './components/Dashboard'
+import BaseFooter from './components/Footer/BaseFooter'
+import BaseNavBar from './components/NavBar/BaseNavBar'
 
 function App() {
   const { user } = useAuthContext()
 
   return (
     <div className='app bg-neutral-900'>
-      {user ? <Dashboard /> : <AuthPage />}
+
+      {user ?   
+        <>        
+          <BaseNavBar />
+          <div className='mt-4'>
+            <Dashboard />
+          </div>
+        </>
+          : <AuthPage />}
+
+      <BaseFooter />
     </div>
   )
 }
