4405638a3b3b23daf5bfe4db5e2a21d92bc7277b
fu-yanyuan <yanyuan@g.ecc.u-tokyo.ac.jp>
2024-08-01 01:20:46 +0900

    feat(ui): add BaseNavBar and BaseFooter

---
+++ src/components/Footer/BaseFooter.jsx
@@ -0,0 +1,12 @@
+const BaseFooter = () => {
+
+  return (
+    <footer className="footer footer-center bg-neutral-900 text-neutral-300 p-4 border-2 border-warning">
+      <aside>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved by codespace 🚀</p>
+      </aside>
+    </footer>
+  )
+}
+
+export default BaseFooter
