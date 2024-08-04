8333b7965fcc011e7468c56ea0dbeba8d6d702ba
fu-yanyuan <yanyuan@g.ecc.u-tokyo.ac.jp>
2024-08-05 01:07:49 +0900

    feat: attempting in Recent

--- src/components/ControlContainer.jsx
+++ src/components/ControlContainer.jsx
@@ -1,26 +1,14 @@
-import AttemptedStack from "./AttemptedStack/AttemptedStack"
 import SearchBar from "./ControlBar/SearchBar"
 import RecentContainer from "./Recent/RecentContainer"
-import { attemptsListener } from "../firebase/firestore"
-import { useState, useEffect } from "react"
 
 const ControlContainer = () => {
-  const [itemLists, setItemLists] = useState(null)
-
-  useEffect(() => {
-    const unsubscribe = attemptsListener(setItemLists)
-  }, [])
-
   return (
     <div className="control-container flex flex-col min-w-[840px]">
       <div>
           <SearchBar />
       </div>
       <div className="flex flex-row mt-4">
-        <div className="flex-auto w-1/3 h-[500px]">
-          <AttemptedStack itemLists={itemLists}/>
-        </div>
-        <div className="flex-auto w-2/3 h-[500px] ml-5">
+        <div className="w-full">
           <RecentContainer />
         </div>
       </div>
