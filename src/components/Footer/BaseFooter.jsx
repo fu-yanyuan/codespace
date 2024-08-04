072a51955a86735815aa5cc3e9dc79684ab2b62e
fu-yanyuan <yanyuan@g.ecc.u-tokyo.ac.jp>
2024-08-05 01:05:37 +0900

    fix(ui): modify footer

--- src/components/Footer/BaseFooter.jsx
+++ src/components/Footer/BaseFooter.jsx
@@ -1,7 +1,7 @@
 const BaseFooter = () => {
 
   return (
-    <footer className="footer footer-center bg-neutral-900 text-neutral-300 p-4 border-2 border-warning">
+    <footer className="footer footer-center bg-neutral-900 text-neutral-300 p-4 mt-40 border-2 border-warning">
       <aside>
         <p>Copyright © {new Date().getFullYear()} - All right reserved by codespace 🚀</p>
       </aside>
